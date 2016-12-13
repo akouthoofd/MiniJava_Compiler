cd17a911ca76ba8e5692cae5ad0cbcc55430f1fd
Alex Kouthoofd <akouthoo@oswego.edu>
2016-12-13 18:51:27 -0500

    Removed everything!

--- src/my/mini/java/compiler/AST/nodes/type/EnterScopeNode.java
+++
@@ -1,9 +0,0 @@
-package my.mini.java.compiler.AST.nodes.type;
-
-/**
- * Created by alex on 12/2/2016.
- */
-public class EnterScopeNode extends TypeNode
-{
-
-}
--- src/my/mini/java/compiler/Exceptions/ErrorReporter.java
+++
@@ -1,9 +0,0 @@
-package my.mini.java.compiler.Exceptions;
-
-/**
- * Created by alex on 12/1/2016.
- */
-public class ErrorReporter
-{
-
-}
